a9a369f91cc8a8d8b6af0e138d22ed724a2748ce
Jamie Wilson <jamiewilson@Jamies-MacBook-Pro-2.local>
2013-04-11 22:43:07 +1200

    Single Post Layout
    Updated the single post page to reflect the style of the loop.

--- single.php
+++ single.php
@@ -9,57 +9,37 @@
 		
 			<div class="container clearfix">
 
-			<div class="col col1 gutter"></div>
+				<div class="col col1 gutter"></div>
 
-			<div class="col col8 content">
-				<div class="header">
-					<!-- Post Title -->
-					<h2>
-						<?php the_title(); ?>
-						<!--<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">	</a>-->
-					</h2>
-					
-					<!-- Post Details
-					<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-					<span class="comments"><?php comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-					/Post Details -->
+				<div class="col col8 content">
+					<div class="header">
+						<h2>
+							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+						</h2>					
+					</div>									
+				</div>
+
+				<div class="col col2 sidebar"></div>
+				<div class="col col1 gutter"></div>
+
+			</div>
+			<div class="container clearfix">
+
+				<div class="col col1 gutter"></div>
+
+				<div class="col col8 content">
+					<?php the_content(); // Dynamic Content ?>
 					
 				</div>
 
-				<!-- Post Thumbnail -->
-				<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-						<?php the_post_thumbnail(); // Fullsize image for the single post ?>
-					</a>
-				<?php endif; ?>
-				<!-- /Post Thumbnail -->
-				
-				
-				
-				
-				<?php the_content(); // Dynamic Content ?>
-				
-				
-				<br class="clear">
-				
-				<?php //the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
-				
-		<!--		<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
-			-->	
-<!--				<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
-	-->			
-				<?php //edit_post_link(); // Always handy to have Edit Post Links available ?>
-				
-				<?php //comments_template(); ?>
-			
-			</div>
 
 			<div class="col col2 sidebar">
 
-				<ul>
-					<li><?php the_time('F j, Y'); ?></li>
-					<li><?php the_time('g:i a'); ?></li>
+				<ul class="icon-list">
+					<li><span class="ss-calendar icon"></span><?php the_time('F j, Y'); ?></li>
+					<li><span class="ss-clock icon"></span><?php the_time('g:i a'); ?></li>
 				</ul>
+
 				<!--<aside>
 					<h3>Sidebar</h3>
 					<p>It's Groundhog Day, which means that January is over. January, of course, is official diet book month, the time of year that formerly young.</p>
